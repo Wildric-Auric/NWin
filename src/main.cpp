69cbda3e3d60b34d402a1e1aef86df6088acce77
Oussama <wildric.auric@gmail.com>
2024-03-07 15:26:33 +0100

    added sample entry point and test for opengl compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
-#include "window.h"
+#include "gl_context.h"
+#include "window.h"   
+#include <windows.h>
+//#include <gl/glu.h>
+//#include <GL/GL.h>
+#include <gl/glew.h>
 
 void resize(NWin::winHandle handle) {
-	printf("ResizeTest\n");
+	//printf("ResizeTest\n");
+}
+
+void draw11(NWin::winHandle h) {
+	NWin::Window* win = NWin::Window::stGetWindow(h);
+	glClearColor(0.3, 0.3, 0.3, 0.0);
+	glViewport(0,0, win->getDrawAreaSize().x,win->getDrawAreaSize().y);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2i(0, 1);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2i(-1, -1);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex2i(1, -1);
+	glEnd();
+	glFlush();
+}
+
+extern void draw(int sizeX, int sizeY);
+extern int init();
+
+void draw3(NWin::winHandle h) {
+	NWin::Window* win = NWin::Window::stGetWindow(h);
+	draw(win->getDrawAreaSize().x, win->getDrawAreaSize().y);
+	win->swapBuffers();
 }
 
 void sample() {
@@ -11,11 +41,19 @@ void sample() {
 	c.metrics.pos = { 0,0 };
 	c.description = "HelloWorld";
 	c.metrics.size = { 480, 360 };
-
+	c.style   = WS_OVERLAPPEDWINDOW;
+	c.exStyle = WS_EX_APPWINDOW;
 	w = NWin::Window::stCreateWindow(c);
 	w->resizeCallback = &resize;
+	w->drawCallback   = &draw3;
+	NWin::GlContext context;
+	context.create(w);
+	context.makeCurrent();
+	context.initCoreOpenGL();
+	init();
 	while (w->shouldLoop()) { w->update(); }
 	NWin::Window::stDestroyWindow(w);
+	context.makeCurrent(1);
 }
 
 int main() {
