627a1b2888b763c8abdbea6bb1191b5648e2129d
Oussama <wildric.auric@gmail.com>
2024-03-07 15:25:00 +0100

    added util for common  functions/macros

--- src/util.h
+++ src/util.h
@@ -2,11 +2,25 @@
 
 #include <iostream>
 namespace NWin {
-	#define display(msg) std::cout << (msg) << "\n";
-	//TODO::Add line and file of the error
-	#define WIN_CHECK2(expr, errorValue) if (((expr)) == errorValue) { display("Error at evaluation expression: " #expr)}
+	//Whatever is wraper of, the object should implement the operator "<<"
+	#define display(msg) std::cout << msg << "\n";
+	#define TO_STR_0(nonStr)   #nonStr
+	#define TO_STR(nonStr) TO_STR_0(nonStr)
 
-	#define WIN_CHECK(expr) WIN_CHECK2(expr, 0)
+	#define WIN_DISPLAY_ERROR(expr) { int win_get_last_error_var; if ((win_get_last_error_var = GetLastError()) != 0) \
+	{display("WARNING: GetLasterror yields: " << win_get_last_error_var << " after using the expr: " #expr "\n");}}
+
+	#define WIN_CHECK_COMPLETE(expr, errorValue, code) if (((expr)) == (errorValue) )  \
+		{ display("\n----------------------------\n" \
+				  "ERROR at file: "  __FILE__  \
+				  "\n\nLine:" TO_STR(__LINE__)  \
+				  "\n\nDuring evaluation of the expression: " #expr "\n\n"); WIN_DISPLAY_ERROR(expr); display("----------------------------\n"); code;} \
+		else { WIN_DISPLAY_ERROR(expr);  }
+
+
+	#define WIN_CHECK2(expr, errorValue) WIN_CHECK_COMPLETE(expr, errorValue,)
+	#define WIN_CHECK21(expr, code)		 WIN_CHECK_COMPLETE(expr,0,code)
+	#define WIN_CHECK(expr)				 WIN_CHECK_COMPLETE(expr, 0,)
 
 	#define DEFAULT_WINDOW_POSITION_SIZE ((int)0x80000000)
 	typedef unsigned long Word;
